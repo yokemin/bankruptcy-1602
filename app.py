4c6d2050ae2ae634665528cd90aa5e03dc85aafe
Yoke Min <46562387+yokemin@users.noreply.github.com>
2022-02-16 17:10:55 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+from flask import Flask
+
+
+# In[2]:
+
+
+app = Flask(__name__)
+
+
+# In[3]:
+
+
+from flask import request, render_template
+from keras.models import load_model
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method=="POST":
+        NPTA = request.form.get("NPTA")
+        TLTA = request.form.get("TLTA")
+        WCTA = request.form.get("WCTA")
+        print(NPTA, TLTA, WCTA)
+        
+        model = load_model("bankruptcy_model")
+        pred = model.predict([[float(NPTA), float(TLTA), float(WCTA)]])
+        s = "The predicted bankruptcy score is " + str(pred)
+        return(render_template("index.html", result=s))
+    else:
+        return(render_template("index.html", result="2"))
+
+
+# In[ ]:
+
+
+if __name__ == "__main__":
+    app.run()
+
+
+# In[ ]:
+
+
+
+
